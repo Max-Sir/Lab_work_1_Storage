a94623d9542baea7d49c8301b8f249c2001c6c01
Maxim <syromolotov.maks@mail.ru>
2020-03-06 13:41:14 +0300

    2nd version of doing it only in pure C 06.03.2020

---
+++ Lab_12_Storage/Source.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+struct Storage
+{
+	union info
+	{
+		struct Auto_Storage
+		{
+			
+		};
+
+		struct Product_Storage
+		{
+			
+		};
+	};
+};
+
+int main(void)
+{
+	
+}
